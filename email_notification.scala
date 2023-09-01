8060e1a2da3ce0bf9fba26de99421cf0d49868d6
modaksanthosh <126580294+modaksanthosh@users.noreply.github.com>
2023-09-01 15:16:39 +0530

    Update email_notification.scala

--- email_notification.scala
+++ email_notification.scala
@@ -1,9 +1,17 @@
 import java.util.{Calendar, Properties}
 import javax.mail.{Message, Session}
 import javax.mail.internet.{InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart}
- 
 
-def main_Notification(Email_port:String,Email_recipient:String,Subject:String,Message_Body:String,Email_host:String, Email_username:String, Email_password:String):Unit ={
+{
+ var Email_host:String = "smtp.gmail.com"
+  var Email_port :String= "587"
+  var Email_username :String= "mallem.santhosh@gmail.com"
+  var Email_password :String= "ohippvtbzbuzfcjr"
+  var Email_recipient:String="ramsai350mg@gmail.com,mallem.santhosh@gmail.com,santhosh.mallem@modak.com"
+  var Subject:String ="Testing the Message Functionality"
+  var Message_Body:String ="Hello Hai This is Testing Related Message"
+
+
 
 val properties = new Properties()
     properties.put("mail.smtp.port", Email_port)
@@ -34,15 +42,4 @@ transport.sendMessage(message, message.getAllRecipients)
 
 }
 
-{
- var Email_host:String = "smtp.gmail.com"
-  var Email_port :String= "587"
-  var Email_username :String= "mallem.santhosh@gmail.com"
-  var Email_password :String= "ohippvtbzbuzfcjr"
-  var Email_recipient:String="ramsai350mg@gmail.com,mallem.santhosh@gmail.com,santhosh.mallem@modak.com"
-  var Subject:String ="Testing the Message Functionality"
-  var Message_Body:String ="Hello Hai This is Testing Related Message"
 
-
-main_Notification(Email_port:String,Email_recipient:String,Subject:String,Message_Body:String,Email_host:String, Email_username:String, Email_password:String)
-}
