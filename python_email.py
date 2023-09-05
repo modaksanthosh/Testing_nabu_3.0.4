e84dca5b67efa0943de21bb11ec9430db6ec0d08
modaksanthosh <126580294+modaksanthosh@users.noreply.github.com>
2023-09-05 17:03:38 +0530

    Update python_email.py

--- python_email.py
+++ python_email.py
@@ -1,13 +1,15 @@
 import smtplib
 import ssl
+print("Hello World")
+
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "mallem.santhosh@gmail.com"
-receiver_email = "ramsai350mg@gmail.com"
+receiver_email = "ramsai350mg@gmail.com,santhosh.mallem@modak.com"
 password = "ohippvtbzbuzfcjr"
 message = """\
 Subject: Hi there
-Im sending an email through python code."""
+Im sending an email through python code and from Modak Nabu."""
 context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo() 
@@ -15,3 +17,4 @@ with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo() 
     server.login(sender_email, password)
     server.sendmail(sender_email, receiver_email, message)
+print("SuccessFully Send Message 1000")
