c22532adb1b98e9ef5350a9e5edd87d9b2ee43d3
modaksanthosh <126580294+modaksanthosh@users.noreply.github.com>
2023-09-05 19:53:58 +0530

    Update email_notification.scala

--- email_notification.scala
+++ email_notification.scala
@@ -1,49 +1,43 @@
-import java.util.{Calendar, Properties}
-import javax.mail.{Message, Session}
-import javax.mail.internet.{InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart}
+import javax.mail._
+import javax.mail.internet._
 
- var Email_host:String = "smtp.gmail.com"
- var Email_port :String= "25"
- var Email_username :String= "mallem.santhosh@gmail.com"
- var Email_password :String= "ohippvtbzbuzfcjr"
- var Email_recipient:String="ramsai350mg@gmail.com,mallem.santhosh@gmail.com,santhosh.mallem@modak.com"
- var Subject:String ="Testing the Message Functionality"
- var Message_Body:String ="Hello Hai This is Testing Related Message"
+val host = "smtp.gmail.com"
+val port = "587" // Port for TLS
+val username = "mallem.santhosh@gmail.com"
+val password = "ohippvtbzbuzfcjr"
 
-// props.put("mail.smtp.auth", "true");
-//     props.put("mail.smtp.starttls.enable", "true"); 587
-//     props.put("mail.smtp.host", "smtp.gmail.com");
-//     props.put("mail.smtp.port", "465");
-//     props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+val from = new InternetAddress(username)
+val to = new InternetAddress("ramsai350mg@gmail.com")
 
+    // Email properties
+val props = new java.util.Properties()
+props.put("mail.smtp.host", host)
+props.put("mail.smtp.port", port)
+props.put("mail.smtp.auth", "true")
+props.put("mail.smtp.starttls.enable", "true")
 
-val properties = new Properties()
-    properties.put("mail.smtp.port", Email_port)
-    properties.put("mail.smtp.auth", "true")
-    properties.put("mail.smtp.host", "smtp.gmail.com")
-    properties.put("mail.smtp.ssl.enable", "false")
-    properties.put("mail.smtp.ssl.protocols", "TLSv1.2");
-      properties.put("mail.smtp.starttls.enable", "true")
-      // properties.put("mail.smtp.ssl.trust", Email_host)
-      
+    // Create a session with authentication
+val session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+        override protected def getPasswordAuthentication: PasswordAuthentication = {
+          new PasswordAuthentication(username, password)
+        }
+      })
 
- val session = Session.getDefaultInstance(properties)
- val message = new MimeMessage(session)
- val recipientList:List[String] = Email_recipient.split(",").toList
-    recipientList.foreach { recipient =>
-      message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient))
+    try {
+      // Create a message
+      val message = new MimeMessage(session)
+      message.setFrom(from)
+      message.setRecipient(Message.RecipientType.TO, to)
+      message.setSubject("Hello, Scala Email!")
+      message.setText("This is a test email sent from Scala.")
+
+      // Send the message
+      Transport.send(message)
+
+      println("Email sent successfully!")
+    } catch {
+      case e: MessagingException =>
+        e.printStackTrace()
+        println("Email could not be sent.")
     }
-
-message.setSubject(Subject)
-
-val msg =Message_Body
-
-val MessagePart = new MimeMultipart()
-      val BodyPartText = new MimeBodyPart()
-            BodyPartText.setText(msg,"utf-8","html")
-      MessagePart.addBodyPart(BodyPartText)
-      message.setContent(MessagePart)
-
-val transport = session.getTransport("smtp")
-transport.connect(Email_host, Email_username, Email_password)
-transport.sendMessage(message, message.getAllRecipients)
