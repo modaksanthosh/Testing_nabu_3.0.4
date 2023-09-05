f1ecf4b6d24950b0f332f3608307f0a79aa4fd21
modaksanthosh <126580294+modaksanthosh@users.noreply.github.com>
2023-09-05 17:52:00 +0530

    Update python_email.py

--- python_email.py
+++ python_email.py
@@ -1,20 +1,24 @@
+
+# Nabu executable Code in pyspark
 import smtplib
 import ssl
-print("Hello World")
+def Email_Notification_Using_PySpark(smtp_server,port,receiver_email,sender_email,password,message):
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo() 
+        server.starttls(context=context)
+        server.ehlo() 
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
 
+
+# Calling Email_Notification
 port = 587  # For starttls
 smtp_server = "smtp.gmail.com"
 sender_email = "mallem.santhosh@gmail.com"
-receiver_email = "ramsai350mg@gmail.com,santhosh.mallem@modak.com"
+receiver_email = ["ramsai350mg@gmail.com","mallem.santhosh@gmail.com","santhosh.mallem@modak.com"]
 password = "ohippvtbzbuzfcjr"
 message = """\
 Subject: Hi there
-Im sending an email through python code and from Modak Nabu."""
-context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.ehlo() 
-    server.starttls(context=context)
-    server.ehlo() 
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
-print("SuccessFully Send Message 1000")
+Im sending an email through python code Data Bricks"""
+Email_Notification_Using_PySpark(smtp_server,port,receiver_email,sender_email,password,message)
